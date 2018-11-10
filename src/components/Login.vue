940b1c7dacebc72db0bdaab1845ded1bb08c6aeb
guiamado <amado.silva133@gmail.com>
2018-11-10 18:16:44 -0200

    [FIX] Tira erro de colocar name no componente

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -64,7 +64,6 @@
 </template>
 <script>
   export default {
-    name: ['Login'],
     data: () => ({
       whatsNext: [
         {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
 
   export default {
     components: {
-      Login
+      Login,
     },
   };
 </script>
